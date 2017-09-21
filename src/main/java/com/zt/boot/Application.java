57171ecd8887b7a913385c72e0fe68c42ea05924
周涛 <zhoutao@chuangjiangx.com>
2017-09-21 21:27:32 +0800

    11

--- src/main/java/com/zt/boot/Application.java
+++ src/main/java/com/zt/boot/Application.java
@@ -1,11 +1,7 @@
 package com.zt.boot;
 
-import com.zt.boot.configuration.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Created by zhoutao on 17/5/28.
